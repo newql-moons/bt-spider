8856cded702ba199ce4724b23a5bbeac1fd929d2
newql-moons <1156325827@qq.com>
2017-01-28 18:43:45 +0800

    Alpha

--- dht.py
+++ dht.py
@@ -79,7 +79,7 @@ class Spider(multiprocessing.Process):
                     self.route_table.insert(node)
         except KeyError:
             logging.warning('Unknown msg: [%s] from [%s:%d]' % (dict(msg), addr[0], addr[1]))
-            logging.warning(sys.exc_info()[:2])
+            # logging.warning(sys.exc_info()[:2])
 
     def req_handler(self, t, q, a, node):
         if node not in self.route_table:
