e4502574bd1ec108e0206da88fb5a03a900b186c
newql-moons <1156325827@qq.com>
2017-01-28 10:58:44 +0800

    multi spider

--- launch.py
+++ launch.py
@@ -4,7 +4,7 @@ from dht import Spider
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG,
+    logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
                         datefmt='%a, %d %b %Y %H:%M:%S', )
     spiders = []
